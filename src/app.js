0a92530a9d8a7cfea55271cd1c883df5baa81a07
“Josue_Cardenas” <josuechatto3010@gmail.com>
2023-12-09 09:18:56 -0600

    alter apps

--- src/app.js
+++ src/app.js
@@ -1,35 +1,52 @@
-//const http= require('http');
 const express = require('express');
-const app= express();
+const app = express();
 const puerto = 80;
 
-const MenusController=require('./controllers/MenuControllers');
-app.use(express.json())
-app.get('/menus',MenusController.indexGet);
-app.get('/menus/:id([0-9]+)', MenusController.ItemGet);
-//app.get('/menus/:id)', MenusController.getMenuById);
-
-const productos_tiposController=require('./controllers/productos_tiposControllers');
-app.use(express.json())
-app.get('/productos_tipos',productos_tiposController.indexGet);
-
-const productos_Controller=require('./controllers/productosControllers');
-app.use(express.json())
-app.get('/productos',productos_Controller.indexGet);
-
-const categorias_Controller=require('./controllers/categoriasController');
-app.use(express.json())
-app.get('/categorias',categorias_Controller.indexGet);
-app.get('/categorias/:id([0-9]+)', categorias_Controller.ItemGet);
+app.use(express.json());
 
 
-app.get('/', function(req,res) {
-    res.statusCode=200;
+// Importa los controladores correspondientes
+const UsuariosController = require('./controllers/UsuariosControllers');
+const CuestionariosController = require('./controllers/CuestionariosControllers');
+const DificultadesController = require('./controllers/DificultadesControllers');
+const PuntosProgController = require('./controllers/PuntosProgControllers');
+
+// Endpoints para la tabla usuarioj
+app.get('/usuarios', UsuariosController.indexGet);
+app.get('/usuarios/:id([0-9]+)', UsuariosController.ItemGet);
+app.post('/usuarios', UsuariosController.indexPost);
+app.put('/usuarios/:id([0-9]+)', UsuariosController.itemPut);
+app.patch('/usuarios/:id([0-9]+)', UsuariosController.itemPatch);
+
+// Endpoints para la tabla cuestionario
+app.get('/cuestionarios', CuestionariosController.indexGet);
+app.get('/cuestionarios/:id([0-9]+)', CuestionariosController.ItemGet);
+app.post('/cuestionarios', CuestionariosController.indexPost);
+app.put('/cuestionarios/:id([0-9]+)', CuestionariosController.itemPut);
+app.patch('/cuestionarios/:id([0-9]+)', CuestionariosController.itemPatch);
+
+// Endpoints para la tabla dificultad
+app.get('/dificultades', DificultadesController.indexGet);
+app.get('/dificultades/:id([0-9]+)', DificultadesController.ItemGet);
+app.post('/dificultades', DificultadesController.indexPost);
+app.put('/dificultades/:id([0-9]+)', DificultadesController.itemPut);
+app.patch('/dificultades/:id([0-9]+)', DificultadesController.itemPatch);
+
+// Endpoints para la tabla puntosProg
+app.get('/puntosProg', PuntosProgController.indexGet);
+app.get('/puntosProg/:id([0-9]+)', PuntosProgController.ItemGet);
+app.post('/puntosProg', PuntosProgController.indexPost);
+app.put('/puntosProg/:id([0-9]+)', PuntosProgController.itemPut);
+app.patch('/puntosProg/:id([0-9]+)', PuntosProgController.itemPatch);
+
+app.get('/', function (req, res) {
+    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hola mundo');
+    res.send('Bienvenido a EstateIQ!');
 });
 
-app.listen(puerto, function(){
-    console.log('Servidor iniciado');
+app.listen(puerto, function () {
+    console.log("Servidor iniciado");
 });
+
 
